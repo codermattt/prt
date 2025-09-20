b31ede5cd6080c97eb77492116aa0d5484cd1a6a
codermattt <codermattt@gmail.com>
2025-09-20 11:35:52 +0200

    first branch commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.printf("Hello and welcome!");
+    }
+    private static String a(){
+        return "mathew";
+    }
+    private static String b(){
+        return "is here";
+    }
+
+}
